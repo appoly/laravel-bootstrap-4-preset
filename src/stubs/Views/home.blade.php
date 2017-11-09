9d7b3526800161a25cb156516d797f511fbbf416
James Merrix <james.merrix@gmail.com>
2017-11-09 13:55:54 +0000

    Fixes to the home screen layout and code formatting clean up.

--- src/stubs/Views/home.blade.php
+++ src/stubs/Views/home.blade.php
@@ -2,17 +2,15 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-md-center mt-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Dashboard</h4>
-                        <p class="card-text">You are logged in!</p>
-                    </div>
-                </div>
+<div class="row justify-content-md-center mt-5">
+    <div class="col-md-8">
+        <div class="card">
+            <h4 class="card-header">Dashboard</h4>
+            <div class="card-body">
+                <p class="card-text">You are logged in!</p>
             </div>
         </div>
     </div>
-
+</div>
+    
 @endsection
